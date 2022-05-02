8f477ec4ccf41bf044c9358ad8b26453723f724d
Mereko Febbi <mirkofebbo@gmail.com>
2022-05-02 09:44:17 +0100

    name update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -31,8 +31,7 @@ function Navbar() {
       <nav className='navbar'>
         <div className='navbar-container'>
         <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-            Mereko Febbi   
-   
+            Transparency   
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
